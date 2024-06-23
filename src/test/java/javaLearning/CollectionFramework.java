a0a553c38d4c76a7831353385592173fa1532cee
Engineerajay <ajayingole44@gmail.com>
2024-06-23 15:49:48 +0530

    collection framework class added

---
+++ src/test/java/javaLearning/CollectionFramework.java
@@ -0,0 +1,10 @@
+package javaLearning;
+
+public class CollectionFramework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("collection framework");
+	}
+
+}
