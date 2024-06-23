0a51ebef367e334fab3c7151c704b56c59301323
swapnil <swapniljagtap293@gmail.com>
2024-06-23 15:42:58 +0530

    list interface class added

---
+++ src/test/java/javaLearning/List_Interface.java
@@ -0,0 +1,11 @@
+package javaLearning;
+
+public class List_Interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("List Interface");
+
+	}
+
+}
