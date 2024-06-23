281bb7efa0b53f4d81d6a0a9dbdcf562be0415a0
Engineerajay <ajayingole44@gmail.com>
2024-06-23 16:17:38 +0530

    collection framework file update

--- src/test/java/javaLearning/CollectionFramework.java
+++ src/test/java/javaLearning/CollectionFramework.java
@@ -4,7 +4,8 @@ public class CollectionFramework {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("collection framework");
+		System.out.println("collection framework");
+		System.out.println("list ,set");
 	}
 
 }
