8bb335ea952c6466148f31078674383c2b29fe7f
swapnil <swapniljagtap293@gmail.com>
2024-06-23 12:22:54 +0530

    SA_Framework First Commit

---
+++ src/test/java/javaLearning/ArrayDemo.java
@@ -0,0 +1,18 @@
+package javaLearning;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		String[] arr = { "Ajay", "Swapnil", "Nana" };
+
+	//	System.out.println(arr[0]);
+		
+		for(String a : arr)
+		{
+			System.out.println(a);
+		}
+
+	}
+
+}
